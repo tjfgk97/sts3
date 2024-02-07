5f01e60a87929109543f787f121ea4411dbeeecb
tjfgk97 <tjfgk97@gmail.com>
2024-02-07 16:34:04 +0900

    commit_240207

--- movieinfo/src/main/java/com/icia/movieinfo/HomeController.java
+++ movieinfo/src/main/java/com/icia/movieinfo/HomeController.java
@@ -1,10 +1,19 @@
 package com.icia.movieinfo;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.icia.movieinfo.dto.MovieDto;
 import com.icia.movieinfo.service.MovieService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -12,17 +21,38 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @Slf4j
 public class HomeController {
-	
 	@Autowired
 	private MovieService mServ;
-
-	@GetMapping("/")
-	public String home(Model model) {
-		log.info("home()");
 	
-		String view = mServ.getMovieList(model);
+	@GetMapping("/")
+	public String home(Integer pageNum, 
+						Model model, 
+						HttpSession session) {
+		log.info("home()");
+		
+		String view = mServ.getMovieList(pageNum, model, session);
 		
 		return view;
 	}
 	
-}// class end
+	@GetMapping("writeFrm")
+	public String writeFrm() {			
+		log.info("writeFrm()");
+		return "writeFrm";
+	}
+	
+	@PostMapping("writeProc")
+	public String writeProc(@RequestPart List<MultipartFile> files,
+								MovieDto movie,
+								HttpSession session,
+								RedirectAttributes rttr) {
+		log.info("writeProc()");
+		String view = mServ.insertMovie(files, movie, session, rttr);
+		return view;
+		
+	}
+	
+}//class end
+
+
+
--- movieinfo/src/main/java/com/icia/movieinfo/dto/MovieDto.java
+++ movieinfo/src/main/java/com/icia/movieinfo/dto/MovieDto.java
@@ -9,13 +9,17 @@ import lombok.Setter;
 @Setter
 @Alias("movie")
 public class MovieDto {
-    private int m_code; 
-    private String m_name; // 제목
-    private String m_director; // 감독 
-    private String m_nation; // 국가
-    private String m_genre; // 장르
-    private String m_actor; // 주연배우
-    private String m_open; // 개봉일
-    private String m_synopsis; // 영화 개요
-    private String p_sysname; // 포스터 저장이름
+	private int m_code;
+    private String m_name;//제목
+    private String m_director;//감독
+    private String m_nation;//국가
+    private String m_genre;//장르
+    private String m_actor;//주연배우
+    private String m_open;//개봉일
+    private String m_synopsis;//영화 개요
+    private String p_sysname;//포스터 저장이름
 }
+
+
+
+
